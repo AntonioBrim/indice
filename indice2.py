bc2c287fc439664deadf215234b013655475de7a
AntonioBrim <108983674+AntonioBrim@users.noreply.github.com>
2022-08-07 13:29:19 -0300

    Update indice2.py

--- indice2.py
+++ indice2.py
@@ -1,22 +1,22 @@
-numero =int(input("digite o numero desejado "))
+numero = int(input("Digite o numero desejado :"))
+
 fibonacci = 0
 fibonacci_auxiliar = 0
 
-if (numero == 0):
-    print(" o número informado pertence a sequência")
+if(numero == 0):
+    print("o número informado pertence a sequência")
 
 while(numero > fibonacci):
 
     if(fibonacci == 0):
         fibonacci += 1
-
-    fibonacci += fibonacci_auxiliar
-
+   
+    fibonacci = fibonacci + fibonacci_auxiliar
     fibonacci_auxiliar = fibonacci - fibonacci_auxiliar
 
-    if(numero == fibonacci):
-        print(" o número informado pertence a sequência")
+    if(fibonacci == numero):
+        print("o número informado pertence a sequência")
         break
-
-    if(numero < fibonacci):
-        print(" o número informado nao pertence a sequência")
+    
+if(numero < fibonacci):
+    print("o número informado nao pertence a sequência")
